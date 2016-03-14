9b6dab52b0c4af8bba3a96b8a1e45eb4600ea418
Janyk Steenbeek <info@janyksteenbeek.nl>
2016-03-14 14:00:54 +0100

    Implemented fixed Auth middleware
    https://github.com/Sectorr/Sectorr/commit/8bc21237d78c700eff91dbe9830e766d0a061751

--- app/Middleware/Auth.php
+++ app/Middleware/Auth.php
@@ -4,13 +4,12 @@ namespace App\Middleware;
 
 use Sectorr\Core\Contracts\MiddlewareContract;
 use Sectorr\Core\Http\Redirect;
-use Sectorr\Core\Auth\Auth;
 
 class Auth implements MiddlewareContract {
 
     public static function allow()
     {
-        if(Auth::check()) {
+        if(\Sectorr\Core\Auth\Auth::check()) {
             return true;
         }
         return Redirect::route('login');
